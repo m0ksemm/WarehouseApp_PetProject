c44dd8bf014bbc99b5aaa834eb8107311b27f490
m0ksem <md.cheban@gmail.com>
2025-10-02 19:50:43 +0300

    AddedWarehousesController

---
+++ WarehouseWebAPI/WarehouseWebAPI/Controllers/WarehousesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WarehouseWebAPI.Filters;
+
+namespace WarehouseWebAPI.Controllers
+{
+    [ApiController]
+    [TypeFilter(typeof(HandleExceptionFilter))]
+    [Route("[controller]")]
+    public class WarehousesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
