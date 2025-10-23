5edaab470c9c3ff74ab266aed565f3126c4fa831
m0ksem <md.cheban@gmail.com>
2025-10-23 19:34:23 +0300

    Added some DTOs

---
+++ WarehouseApp/APIServiceContracts/DTOs/CategoriesDTOs/CategoryResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIServiceContracts.DTOs.CategoriesDTOs
+{
+    public class CategoryResponse
+    {
+        public Guid CategoryID { get; set; }
+        public string? CategoryName { get; set; }
+    }
+}
---
+++ WarehouseApp/APIServiceContracts/DTOs/ManufacturersDTOs/ManufacturerResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIServiceContracts.DTOs.ManufacturersDTOs
+{
+    public class ManufacturerResponse
+    {
+        public Guid ManufacturerID { get; set; }
+        public string? ManufacturerName { get; set; }
+        public int Deliveries { get; set; }
+    }
+}
